021b75cdc733a821c0911f2ba05f3f21e50f6938
Saken-Tukenov <saken@tukenov.kz>
2024-01-08 19:38:33 +0600

    Added Loging

--- src/dataset_preparer.py
+++ src/dataset_preparer.py
@@ -1,5 +1,6 @@
 
 import os
+import logging
 import src.utils as utils
 
 
@@ -8,18 +9,27 @@ class DatasetPreparer:
         self.dataset_name = dataset_name
         self.files = files
         self.dataset_dir = f"data/{self.dataset_name}/"
+        logging.basicConfig(level=logging.INFO)
 
     def download(self):
+        logging.info(f"Starting download of files for dataset: {self.dataset_name}")
         os.makedirs(self.dataset_dir, exist_ok=True)
         for file_url in self.files:
             filename = file_url.split('/')[-1]
             filepath = f"{self.dataset_dir}{filename}"
             if not os.path.isfile(filepath) or os.path.getsize(filepath) == 0:
+                logging.info(f"Downloading file: {filename}")
                 utils.download_file(file_url, filepath)
+            else:
+                logging.info(f"File already exists and is not empty: {filename}")
 
     def merge(self):
+        logging.info(f"Starting merge of CSV files in {self.dataset_dir}")
         utils.merge_csv_files(f"{self.dataset_dir}*.csv", f"{self.dataset_dir}{self.dataset_name}.csv")
+        logging.info(f"Merge completed for dataset: {self.dataset_name}")
 
     def run(self):
+        logging.info(f"Running dataset preparation for: {self.dataset_name}")
         self.download()
         self.merge()
+        logging.info(f"Dataset preparation completed for: {self.dataset_name}")
